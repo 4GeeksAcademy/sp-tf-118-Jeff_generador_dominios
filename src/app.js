a7c9f9c540411be070e8936094478d9e2c9abf73
Hun73R717 <headhunter717@gmail.com>
2025-08-08 16:10:16 +0000

    cambios en el foreach y añadir ciclo dominio

--- src/app.js
+++ src/app.js
@@ -14,18 +14,23 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let dom = ['.com', '.es', '.eu', '.ol'];
-const arreg = [pronoun, adj, noun,, dom]
+const arreg = [pronoun, adj, noun, , dom]
 
 // const mix = arreg => {
 //   const index = Math.floor(Math.random()*arreg.length)
 //   return index
 // } 
-for (let p of pronoun) {
-    for (let a of adj) {
-        for (let n of noun) {
-          for (let d of dom) {
-            console.log(p + a + n + d);
-          }
-        }
-    }
-}
+
+pronoun.forEach(p => {
+  adj.forEach(a => {
+    noun.forEach(n => {
+      dom.forEach(d => {
+        console.log(p + a + n + d);
+      })
+    })
+  })
+})
+
+
+
+
